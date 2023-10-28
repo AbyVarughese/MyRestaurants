362cc673b67fe49b0f1623a9b2088510a9548933
Aby Varughese <abyasnu@gmail.com>
2023-10-28 11:33:48 +0100

    Refactor to use views from start

---
+++ app/src/main/java/com/example/myrestaurants/main/MainApp.kt
@@ -0,0 +1,10 @@
+package com.example.myrestaurants.main
+
+import android.app.Application
+
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/example/myrestaurants/views/restaurants/RestaurantsView.kt
+++ app/src/main/java/com/example/myrestaurants/views/restaurants/RestaurantsView.kt
@@ -1,9 +1,10 @@
-package com.example.myrestaurants
+package com.example.myrestaurants.views.restaurants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myrestaurants.R
 
-class MainActivity : AppCompatActivity() {
+class RestaurantsView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
