42c8072ba3217aae04b8d87fb65b07960fdcc1bd
Aby Varughese <abyasnu@gmail.com>
2023-10-28 15:15:53 +0100

    add menu and button

--- app/src/main/java/com/example/myrestaurants/views/restaurants/RestaurantsView.kt
+++ app/src/main/java/com/example/myrestaurants/views/restaurants/RestaurantsView.kt
@@ -2,11 +2,29 @@ package com.example.myrestaurants.views.restaurants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
 import com.example.myrestaurants.R
+import com.example.myrestaurants.databinding.ActivityRestaurantsBinding
 
 class RestaurantsView : AppCompatActivity() {
+    private lateinit var binding: ActivityRestaurantsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityRestaurantsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        binding.fabAdd.setOnClickListener {
+            val greetingText = getString(R.string.greeting_text)
+            Toast.makeText(applicationContext, greetingText, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
