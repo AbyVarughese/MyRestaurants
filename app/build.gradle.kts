eb0a2da1610aac8e3537222dcf9b12d3c3877ec5
Aby Varughese <abyasnu@gmail.com>
2023-10-28 11:45:17 +0100

    Add Timber

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.myrestaurants"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myrestaurants"
@@ -41,6 +41,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation("com.github.ajalt:timberkt:1.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/myrestaurants/main/MainApp.kt
+++ app/src/main/java/com/example/myrestaurants/main/MainApp.kt
@@ -1,10 +1,15 @@
 package com.example.myrestaurants.main
 
 import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.Forest.i
 
 class MainApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        i("App started successfully")
+
     }
 }
